2cde40633a7ca9860f72816ee246519ac8cacf10
Ca10San <caiodarkus2@gmail.com>
2019-11-10 15:24:00 -0300

    I made my first project in React

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -17,28 +17,87 @@ export default class Calculator extends Component{
 
     state = {...initialState};
 
-    clearMemory = () => {
+    constructor(props){
+        super(props);
+
+        this.clearMemory = this.clearMemory.bind(this);
+        this.setOperator = this.setOperator.bind(this);
+        this.addDigit = this.addDigit.bind(this);
+    }
+
+    clearMemory(){
         // this set the application to the initial state constant
         this.setState({...initialState});
     };
 
-    setOperator = (operator) =>{
-        console.log(operator);
+    setOperator(operator){
+        if(this.state.current === 0){
+            this.setState({operation: operator, current: 1, clearDisplay: true});
+        }else{
+            const currentOperation = this.state.operation;
+            const values = this.state.values;
+
+            const newValues = this.operatorCalc(currentOperation, ...values);
+          
+            this.setState({
+                values: newValues, 
+                clearDisplay: true, 
+                current: 0, 
+                displayValue: newValues[0],
+                operation: operator
+            });
+        };
     };
 
-    addDigit = (n) => {
+    operatorCalc(operator, ...values){
+        switch (operator) {
+            case "*":
+                values[0] = values[0] * values[1];
+            break;
+
+            case "+":
+                values[0] = values[0] + values[1];
+            break;
+
+            case "-":
+                values[0] = parseFloat(values[0]) - parseFloat(values[1]);
+            break;
+
+            case "/":
+                values[0] = values[0] / values[1];
+            break;
+
+            default:
+                return values;        
+            
+        }
+
+        values[1] = 0;
+        return values;
+    }
+
+    addDigit(n){
         if (n === '.' && this.state.displayValue.includes('.')){
             return;
         };
 
         // the value in display equals to 0 or this variable is false
-        const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay;
+        const clearDisplay = this.state.displayValue === 0 || this.state.clearDisplay;
 
         // if clearDisplay is true then clear display, if it's false receives the value in display instead
         const currentValue = clearDisplay ? '' : this.state.displayValue;
-        const displayValue = currentValue + n;
+        const displayValue = (currentValue === '0') ? 0 : currentValue + n;
         // não esquecer de tentar fazer isso com hook
-        this.setState({displayValue, clearDisplay: false})
+        this.setState({displayValue, clearDisplay: false});
+
+        if( n !== '.'){
+            const i = this.state.current;
+            const newValue = parseFloat(displayValue);
+            const values = this.state.values;
+            values[i] = newValue;
+            this.setState({values});
+            console.log(values);
+        }
     };
 
     render(){
@@ -62,7 +121,6 @@ export default class Calculator extends Component{
                 <Button label="0" double click={this.addDigit} />
                 <Button label="." click={this.addDigit}/>
                 <Button label="=" operator click={this.setOperator}/>
-                
             </div>
         );
     };
